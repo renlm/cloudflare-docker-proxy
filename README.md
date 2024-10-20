a2ce0c0572b9e0f25f63595d3079ed9d942606a4
renlm-a <renlm-a@glodon.com>
2024-10-20 19:32:40 +0800

    up

--- README.md
+++ README.md
@@ -32,6 +32,7 @@
    const routes = {
      "docker-io.renlm.cn": "https://registry-1.docker.io",
      "api-sdkman-io.renlm.cn": "https://api.sdkman.io",
+     "oracle-io.renlm.cn": "https://container-registry.oracle.com",
      "gcr-io.renlm.cn": "https://gcr.io",
      "ghcr-io.renlm.cn": "https://ghcr.io",
      "quay-io.renlm.cn": "https://quay.io",
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const dockerHub = "https://registry-1.docker.io";
 const routes = {
   "docker-io.renlm.cn": dockerHub,
   "api-sdkman-io.renlm.cn": "https://api.sdkman.io",
+  "oracle-io.renlm.cn": "https://container-registry.oracle.com",
   "gcr-io.renlm.cn": "https://gcr.io",
   "ghcr-io.renlm.cn": "https://ghcr.io",
   "quay-io.renlm.cn": "https://quay.io",
